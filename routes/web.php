ba84f5a367fa2a554aa18c39bbbfdbc7cf85070a
maksiuP <maksiu@npng.pl>
2019-08-26 07:46:51 +0200

    add autorization

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);
 
 Route::redirect('/', '/bajki')->name('home');
 
 Route::get('/bajki', 'TaleController@index')->name('tales.index');
-Route::get('/bajki/create', 'TaleController@create')->name('tales.create');
-Route::post('/bajki', 'TaleController@store')->name('tales.store');
 Route::get('/bajki/{tale}', 'TaleController@show')->name('tales.show');
-Route::get('/bajki/{tale}/edit', 'TaleController@edit')->name('tales.edit');
-Route::match(['put', 'patch'], '/bajki/{photo}', 'TaleController@update')->name('tales.update');
-Route::delete('/bajki/{photo}', 'TaleController@destroy')->name('tales.destroy');
 
 Route::get('/artysci', 'ArtistController@index')->name('artists.index');
-Route::get('/artysci/create', 'ArtistController@create')->name('artists.create');
-Route::post('/artysci', 'ArtistController@store')->name('artists.store');
 Route::get('/artysci/{artist}', 'ArtistController@show')->name('artists.show');
-Route::get('/artysci/{artist}/edit', 'ArtistController@edit')->name('artists.edit');
-Route::match(['put', 'patch'], '/artysci/{artist}', 'ArtistController@update')->name('artists.update');
-Route::delete('/artysci/{artist}', 'ArtistController@destroy')->name('artists.destroy');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/bajki/create', 'TaleController@create')->name('tales.create');
+    Route::post('/bajki', 'TaleController@store')->name('tales.store');
+    Route::get('/bajki/{tale}/edit', 'TaleController@edit')->name('tales.edit');
+    Route::match(['put', 'patch'], '/bajki/{photo}', 'TaleController@update')->name('tales.update');
+    Route::delete('/bajki/{photo}', 'TaleController@destroy')->name('tales.destroy');
+
+    Route::get('/artysci/create', 'ArtistController@create')->name('artists.create');
+    Route::post('/artysci', 'ArtistController@store')->name('artists.store');
+    Route::get('/artysci/{artist}/edit', 'ArtistController@edit')->name('artists.edit');
+    Route::match(['put', 'patch'], '/artysci/{artist}', 'ArtistController@update')->name('artists.update');
+    Route::delete('/artysci/{artist}', 'ArtistController@destroy')->name('artists.destroy');
+
+});
 
 Route::post('/cache/flush', 'CacheController@flush')->name('cache.flush');
 
