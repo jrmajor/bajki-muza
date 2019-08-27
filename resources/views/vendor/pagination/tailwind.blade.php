e4b82728c94f4cefab657d9ca5596478b7f76455
maksiuP <maksiu@npng.pl>
2019-08-27 05:57:30 +0200

    add pagination margins

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,39 +1,39 @@
 @if ($paginator->hasPages())
-    <ul class="" role="navigation">
+    <ul class="mt-2" role="navigation">
 
         @if ($paginator->onFirstPage())
-            <li class="inline mr-1 text-gray-600">
+            <li class="inline mr-1 px-2 py-1 text-gray-600">
                 <span>&lsaquo;</span>
             </li>
         @else
-            <li class="inline mr-1">
+            <li class="inline mr-1 px-2 py-1">
                 <a href="{{ $paginator->previousPageUrl() }}" rel="prev">&lsaquo;</a>
             </li>
         @endif
 
         @foreach ($elements as $element)
             @if (is_string($element))
-                <li class="inline mr-1 text-gray-600"><span>{{ $element }}</span></li>
+                <li class="inline mr-1 px-2 py-1 text-gray-600"><span>{{ $element }}</span></li>
             @endif
 
 
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="inline mr-1 px-1 bg-gray-800 rounded-full"><span>{{ $page }}</span></li>
+                        <li class="inline mr-1 px-2 py-1 bg-gray-800 rounded-full"><span>{{ $page }}</span></li>
                     @else
-                        <li class="inline mr-1"><a href="{{ $url }}">{{ $page }}</a></li>
+                        <li class="inline mr-1 px-2 py-1"><a href="{{ $url }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
         @endforeach
 
         @if ($paginator->hasMorePages())
-            <li class="inline mr-1">
+            <li class="inline mr-1 px-2 py-1">
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next">&rsaquo;</a>
             </li>
         @else
-            <li class="inline mr-1 text-gray-600">
+            <li class="inline mr-1 px-2 py-1 text-gray-600">
                 <span>&rsaquo;</span>
             </li>
         @endif
