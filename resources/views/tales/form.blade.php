61de3ab262ce68b233c699e5d4a10d361ca1302a
maksiuP <maksiu@npng.pl>
2020-08-31 23:42:18 +0200

    bug fix

--- resources/views/tales/form.blade.php
+++ resources/views/tales/form.blade.php
@@ -75,14 +75,15 @@
                 <div class="flex-none bg-gray-400 w-10 h-10"
                     style="background-image: url(&quot;data:image/svg+xml;utf8,%3Csvg viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' d='M20 34.5c8.008 0 14.5-6.492 14.5-14.5S28.008 5.5 20 5.5 5.5 11.992 5.5 20 11.992 34.5 20 34.5zm0 1.5c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16z M20 24.5a4.5 4.5 0 100-9 4.5 4.5 0 000 9zm0 1.5a6 6 0 100-12 6 6 0 000 12z M21.25 20a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z M11.345 30.061l4.1-4.746c-.27-.23-.52-.481-.751-.75L9.98 28.623c.418.513.875.994 1.365 1.44zM30.06 11.344a13.34 13.34 0 00-1.436-1.363l-4.048 4.722c.261.225.505.47.73.731l4.754-4.09z' fill='%23858c99'/%3E%3C/svg%3E&quot;)">
                     @if ($tale->cover())
-                        <img src="{{ $tale->cover('128') }}" class="w-10 h-10 object-cover bg-cover"
+                        <img src="{{ $tale->cover('128') }}"
+                            loading="lazy"
+                            class="w-10 h-10 object-cover bg-cover"
                             style="background-image: url(&quot;{{ $tale->cover_placeholder }}&quot;)"
                             x-show="cover.file == '' && cover.remove == 0">
                     @endif
                     <template x-if="cover.file != ''">
                         <img :src="cover.preview"
-                            loading="lazy"
-                            class="w-10 h-10 object-cover transition-opacity duration-200 opacity-0">
+                            class="w-10 h-10 object-cover">
                     </template>
                 </div>
                 <span class="px-3 py-2">
