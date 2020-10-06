e510ac11e76150778ea0a33cbd2b4ba2f4be8419
maksiuP <maksiu@npng.pl>
2020-10-06 04:33:45 +0200

    fix slow tests

--- tests/Feature/Artists/ViewArtistTest.php
+++ tests/Feature/Artists/ViewArtistTest.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Models\Artist;
+use Facades\App\Services\Discogs;
+use Facades\App\Services\Wikipedia;
 use function Pest\Laravel\get;
 
 it('works', function () {
     $artist = Artist::factory()->create();
+
+    Wikipedia::shouldReceive('extract')
+        ->andReturn('test');
+
+    Discogs::shouldReceive('photos')
+        ->andReturn([]);
 
     get("artysci/$artist->slug")
         ->assertOk();
--- tests/Feature/Tales/ViewTaleTest.php
+++ tests/Feature/Tales/ViewTaleTest.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Models\Tale;
+use Facades\App\Services\Discogs;
+use Facades\App\Services\Wikipedia;
 use function Pest\Laravel\get;
 
 it('works', function () {
     $tale = Tale::factory()->create();
+
+    Wikipedia::shouldReceive('extract')
+        ->andReturn('test');
+
+    Discogs::shouldReceive('photos')
+        ->andReturn([]);
 
     get("bajki/$tale->slug")
         ->assertOk();
