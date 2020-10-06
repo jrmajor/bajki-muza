a6795b4a01d6c662b528c8969005a1d43d054b09
maksiuP <maksiu@npng.pl>
2020-10-06 13:42:59 +0200

    fix test

--- tests/Unit/Services/FilmPolskiTest.php
+++ tests/Unit/Services/FilmPolskiTest.php
@@ -3,6 +3,6 @@
 use Facades\App\Services\FilmPolski;
 
 it('can create artist url', function () {
-    expect(Discogs::url('112891'))
+    expect(FilmPolski::url('112891'))
         ->toBe('http://www.filmpolski.pl/fp/index.php?osoba=112891');
 });
