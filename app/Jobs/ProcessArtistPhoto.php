e1a8d2c7f925545820142da6d5d4887c93dd7ef0
maksiuP <maksiu@npng.pl>
2020-10-07 23:00:30 +0200

    add artist photo processor

---
+++ app/Jobs/ProcessArtistPhoto.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Artist;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use Spatie\Image\Image;
+use Spatie\TemporaryDirectory\TemporaryDirectory;
+
+class ProcessArtistPhoto implements ShouldQueue
+{
+    use ProcessesImages, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $artist;
+
+    protected $filename;
+
+    public static $sizes = [
+        56, // 3.5rem
+        84, // 3.5rem * 1.5
+        112, // 3.5rem * 2
+        160, // 10rem
+        240, // 10rem * 1.5
+        320, // 10rem * 2
+    ];
+
+    public function __construct(Artist $artist)
+    {
+        $this->artist = $artist;
+
+        $this->filename = $this->artist->photo;
+    }
+
+    public function handle(): void
+    {
+        $temporaryDirectory = (new TemporaryDirectory)->create();
+
+        $sourceFile = "photos/original/{$this->filename}";
+
+        $sourceStream = Storage::cloud()->readStream($sourceFile);
+
+        $baseImagePath = $this->copyToTemporaryDirectory($sourceStream, $temporaryDirectory, $this->filename);
+
+        $image = Image::load($baseImagePath);
+
+        $this->artist->forceFill([
+            'photo_width' => $image->getWidth(),
+            'photo_height' => $image->getHeight(),
+            'photo_placeholder' => $this->generateTinyJpg($baseImagePath, 'height', $temporaryDirectory),
+        ])->save();
+
+        foreach (self::$sizes as $size) {
+            $responsiveImagePath = $this->generateResponsiveImage($baseImagePath, $size, 'height', $temporaryDirectory);
+
+            $file = fopen($responsiveImagePath, 'r');
+
+            Storage::cloud()
+                ->put("photos/{$size}/{$this->filename}", $file, 'public');
+        }
+
+        $temporaryDirectory->delete();
+    }
+}
---
+++ tests/Unit/Jobs/ProcessArtistPhotoTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Jobs\ProcessArtistPhoto;
+use App\Models\Artist;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use function Tests\fixture;
+
+it('works', function () {
+    $filename = Str::random('10').'.jpg';
+
+    // Photo by Alberto Bigoni on Unsplash
+    $file = fopen(fixture('Images/photo.jpg'), 'r');
+
+    Storage::cloud()->put("photos/original/$filename", $file, 'public');
+
+    $artist = Artist::factory()
+        ->create(['photo' => $filename]);
+
+    ProcessArtistPhoto::dispatchSync($artist);
+
+    $artist->refresh();
+
+    expect($artist->photo_placeholder)
+        ->toBe(file_get_contents(fixture('Images/photo_placeholder.b64')));
+
+    expect($artist->photo_width)->toBe(640)
+        ->and($artist->photo_height)->toBe(964);
+
+    expect(file_get_contents(storage_path("testing/photos/112/$filename")))
+        ->toBe(file_get_contents(fixture('Images/photo_112.jpg')));
+
+    Storage::cloud()->delete("photos/original/$filename");
+    rmdir(storage_path('testing/photos/original'));
+
+    foreach (ProcessArtistPhoto::$sizes as $size) {
+        expect(Storage::cloud()->delete("photos/$size/$filename"))->toBeTrue();
+        rmdir(storage_path("testing/photos/$size"));
+    }
+});
