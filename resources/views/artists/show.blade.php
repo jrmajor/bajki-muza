f408307f7ad855bb52f6312d155414db61362f3d
maksiuP <maksiu@npng.pl>
2020-10-16 00:31:03 +0200

    extract image placeholders to external css

--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -99,14 +99,9 @@
                     @foreach ($artist->asDirector as $tale)
                         <a href="{{ route('tales.show', $tale) }}"
                             class="w-full h-13 flex items-center bg-gray-50 rounded-lg shadow-lg overflow-hidden">
-                            <div class="flex-none bg-gray-400 bg-cover w-13 h-13"
-                                style="background-image: url(
-                                    @if ($tale->cover)
-                                        &quot;{{ $tale->cover_placeholder }}&quot;
-                                    @else
-                                        &quot;data:image/svg+xml;utf8,%3Csvg viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' d='M20 34.5c8.008 0 14.5-6.492 14.5-14.5S28.008 5.5 20 5.5 5.5 11.992 5.5 20 11.992 34.5 20 34.5zm0 1.5c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16z M20 24.5a4.5 4.5 0 100-9 4.5 4.5 0 000 9zm0 1.5a6 6 0 100-12 6 6 0 000 12z M21.25 20a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z M11.345 30.061l4.1-4.746c-.27-.23-.52-.481-.751-.75L9.98 28.623c.418.513.875.994 1.365 1.44zM30.06 11.344a13.34 13.34 0 00-1.436-1.363l-4.048 4.722c.261.225.505.47.73.731l4.754-4.09z' fill='%23858c99'/%3E%3C/svg%3E&quot;
-                                    @endif
-                                )">
+                            <div class="flex-none bg-placeholder-cover w-13 h-13"
+                                @if ($tale->cover) style="background-image: url(&quot;{{ $tale->cover_placeholder }}&quot;)" @endif
+                                >
                                 @if ($tale->cover)
                                     <img src="{{ $tale->cover('120') }}"
                                         srcset="
@@ -138,14 +133,9 @@
                     @foreach ($artist->asLyricist as $tale)
                         <a href="{{ route('tales.show', $tale) }}"
                             class="w-full h-13 flex items-center bg-gray-50 rounded-lg shadow-lg overflow-hidden">
-                            <div class="flex-none bg-gray-400 bg-cover w-13 h-13"
-                                style="background-image: url(
-                                    @if ($tale->cover)
-                                        &quot;{{ $tale->cover_placeholder }}&quot;
-                                    @else
-                                        &quot;data:image/svg+xml;utf8,%3Csvg viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' d='M20 34.5c8.008 0 14.5-6.492 14.5-14.5S28.008 5.5 20 5.5 5.5 11.992 5.5 20 11.992 34.5 20 34.5zm0 1.5c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16z M20 24.5a4.5 4.5 0 100-9 4.5 4.5 0 000 9zm0 1.5a6 6 0 100-12 6 6 0 000 12z M21.25 20a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z M11.345 30.061l4.1-4.746c-.27-.23-.52-.481-.751-.75L9.98 28.623c.418.513.875.994 1.365 1.44zM30.06 11.344a13.34 13.34 0 00-1.436-1.363l-4.048 4.722c.261.225.505.47.73.731l4.754-4.09z' fill='%23858c99'/%3E%3C/svg%3E&quot;
-                                    @endif
-                                )">
+                            <div class="flex-none bg-placeholder-cover w-13 h-13"
+                                @if ($tale->cover) style="background-image: url(&quot;{{ $tale->cover_placeholder }}&quot;)" @endif
+                                >
                                 @if ($tale->cover)
                                     <img src="{{ $tale->cover('120') }}"
                                         srcset="
@@ -177,14 +167,9 @@
                     @foreach ($artist->asComposer as $tale)
                         <a href="{{ route('tales.show', $tale) }}"
                             class="w-full h-13 flex items-center bg-gray-50 rounded-lg shadow-lg overflow-hidden">
-                            <div class="flex-none bg-gray-400 bg-cover w-13 h-13"
-                                style="background-image: url(
-                                    @if ($tale->cover)
-                                        &quot;{{ $tale->cover_placeholder }}&quot;
-                                    @else
-                                        &quot;data:image/svg+xml;utf8,%3Csvg viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' d='M20 34.5c8.008 0 14.5-6.492 14.5-14.5S28.008 5.5 20 5.5 5.5 11.992 5.5 20 11.992 34.5 20 34.5zm0 1.5c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16z M20 24.5a4.5 4.5 0 100-9 4.5 4.5 0 000 9zm0 1.5a6 6 0 100-12 6 6 0 000 12z M21.25 20a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z M11.345 30.061l4.1-4.746c-.27-.23-.52-.481-.751-.75L9.98 28.623c.418.513.875.994 1.365 1.44zM30.06 11.344a13.34 13.34 0 00-1.436-1.363l-4.048 4.722c.261.225.505.47.73.731l4.754-4.09z' fill='%23858c99'/%3E%3C/svg%3E&quot;
-                                    @endif
-                                )">
+                            <div class="flex-none bg-placeholder-cover w-13 h-13"
+                                @if ($tale->cover) style="background-image: url(&quot;{{ $tale->cover_placeholder }}&quot;)" @endif
+                                >
                                 @if ($tale->cover)
                                     <img src="{{ $tale->cover('120') }}"
                                         srcset="
@@ -216,14 +201,9 @@
                     @foreach ($artist->asActor as $tale)
                         <a href="{{ route('tales.show', $tale) }}"
                             class="w-full h-15 flex items-center bg-gray-50 rounded-lg shadow-lg overflow-hidden">
-                            <div class="flex-none bg-gray-400 bg-cover w-15 h-15"
-                                style="background-image: url(
-                                    @if ($tale->cover)
-                                        &quot;{{ $tale->cover_placeholder }}&quot;
-                                    @else
-                                        &quot;data:image/svg+xml;utf8,%3Csvg viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' d='M20 34.5c8.008 0 14.5-6.492 14.5-14.5S28.008 5.5 20 5.5 5.5 11.992 5.5 20 11.992 34.5 20 34.5zm0 1.5c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16z M20 24.5a4.5 4.5 0 100-9 4.5 4.5 0 000 9zm0 1.5a6 6 0 100-12 6 6 0 000 12z M21.25 20a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z M11.345 30.061l4.1-4.746c-.27-.23-.52-.481-.751-.75L9.98 28.623c.418.513.875.994 1.365 1.44zM30.06 11.344a13.34 13.34 0 00-1.436-1.363l-4.048 4.722c.261.225.505.47.73.731l4.754-4.09z' fill='%23858c99'/%3E%3C/svg%3E&quot;
-                                    @endif
-                                )">
+                            <div class="flex-none bg-placeholder-cover w-15 h-15"
+                                @if ($tale->cover) style="background-image: url(&quot;{{ $tale->cover_placeholder }}&quot;)" @endif
+                                >
                                 @if ($tale->cover)
                                     <img src="{{ $tale->cover('120') }}"
                                         srcset="
