f077e3e248f0fddbdc361421fa122cb44f00165a
maksiuP <jrh.mjr@gmail.com>
2020-12-23 01:44:55 +0100

    remove broken seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(UsersSeeder::class);
-        $this->call(ArtistsSeeder::class);
-        $this->call(TalesSeeder::class);
-        $this->call(TalesLyricistsSeeder::class);
-        $this->call(TalesComposersSeeder::class);
-        $this->call(TalesActorsSeeder::class);
+        throw new CommandNotFoundException('Seeders are not implemented yet.');
     }
 }
