bf71c02087e0f3962f2eb42c79610d1147a966fd
maksiuP <jrh.mjr@gmail.com>
2020-12-26 16:50:54 +0100

    improve toBeModel expectation

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,14 +1,21 @@
 <?php
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Constraint\IsInstanceOf;
 use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class)->in(__DIR__);
 
 expect()->extend('toBeModel', function ($expected) {
-    $this->toBeInstanceOf(Model::class);
+    $this->toMatchConstraint(
+        Assert::logicalOr(
+            new IsInstanceOf(Model::class),
+            new IsInstanceOf(Relation::class),
+        ),
+    );
 
     Assert::assertTrue(
         $this->value->is($expected),
