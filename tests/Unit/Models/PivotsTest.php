b482942ccff04850f2c2150e46619f3ad9d096fd
maksiuP <jrh.mjr@gmail.com>
2021-01-16 13:46:31 +0100

    add test

--- tests/Unit/Models/PivotsTest.php
+++ tests/Unit/Models/PivotsTest.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Models\Actor;
 use App\Models\Credit;
 use App\Values\CreditType;
 
-it('casts values', function () {
+test('actor pivot casts values', function () {
+    $casts = (new Actor())->getCasts();
+
+    expect($casts['credit_nr'])->toBe('int');
+});
+
+test('credit pivot casts values', function () {
     $casts = (new Credit())->getCasts();
 
     expect($casts['type'])->toBe(CreditType::class);
     expect($casts['nr'])->toBe('int');
 });
 
-test('ofType method works', function () {
+test('ofType method works in credit pivot', function () {
     $credit = new Credit([
         'type' => CreditType::text(),
     ]);
