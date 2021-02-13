f89007f4e61cbe97a11c029f5fc40dca08202d08
Jeremiasz Major <jrh.mjr@gmail.com>
2021-02-13 01:07:02 +0100

    add type hint

--- app/Providers/ServicesServiceProvider.php
+++ app/Providers/ServicesServiceProvider.php
@@ -4,16 +4,20 @@ namespace App\Providers;
 
 use App\Services\Discogs;
 use App\Services\Wikipedia;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class ServicesServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->app->singleton(Discogs::class, function (Application $app) {
+            return new Discogs(
+                $app->make('config')->get('services.discogs.token'),
+            );
+        });
+
         $this->app->singleton(Wikipedia::class);
 
-        $this->app->singleton(Discogs::class, function ($app) {
-            return new Discogs($app->config->get('services.discogs.token'));
-        });
     }
 }
