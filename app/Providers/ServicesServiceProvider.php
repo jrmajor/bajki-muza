09223e84b568c0e97c3c5aa4ef6340df4b15525f
Jeremiasz Major <jrh.mjr@gmail.com>
2021-02-18 17:40:48 +0100

    refactor

--- app/Providers/ServicesServiceProvider.php
+++ app/Providers/ServicesServiceProvider.php
@@ -5,18 +5,17 @@ namespace App\Providers;
 use App\Services\Discogs;
 use App\Services\FilmPolski;
 use App\Services\Wikipedia;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class ServicesServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(Discogs::class, function (Application $app) {
-            return new Discogs(
-                $app->make('config')->get('services.discogs.token'),
-            );
-        });
+        $this->app->when(Discogs::class)
+            ->needs('$token')
+            ->giveConfig('services.discogs.token');
+
+        $this->app->singleton(Discogs::class);
 
         $this->app->singleton(Wikipedia::class);
 
