66e9c22af70cb58a6a327930919bb462a143d52e
Jeremiasz Major <jrh.mjr@gmail.com>
2021-03-05 18:52:12 +0100

    remove routes/console.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,13 +11,10 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
     }
 
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
     }
 }
