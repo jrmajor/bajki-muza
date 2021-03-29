acc9da50cae22a01c74f115284a9139acb00c700
Jeremiasz Major <jrh.mjr@gmail.com>
2021-03-29 20:52:18 +0200

    formatting

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,13 @@
 const mix = require('laravel-mix')
 
-mix.js('resources/js/app.js', 'public/js')
-
-mix.postCss('resources/css/style.css', 'public/css', [
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/style.css', 'public/css', [
     require('@tailwindcss/jit'),
     require('autoprefixer'),
-  ]).version()
-
-mix.postCss('node_modules/croppr/src/css/croppr.css', 'public/css')
+  ])
+  .postCss('node_modules/croppr/src/css/croppr.css', 'public/css')
+  .version()
 
 if (!mix.inProduction()) {
   require('laravel-mix-bundle-analyzer')
