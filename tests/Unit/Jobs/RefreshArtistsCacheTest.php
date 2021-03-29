9758728a70bc9fafa806ac2ee546c1870c113177
Jeremiasz Major <jrh.mjr@gmail.com>
2021-03-29 16:38:35 +0200

    add test for RefreshArtistsCache job

---
+++ tests/Unit/Jobs/RefreshArtistsCacheTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Jobs\RefreshArtistsCache;
+use App\Models\Artist;
+use Facades\App\Services\Discogs;
+use Facades\App\Services\FilmPolski;
+use Facades\App\Services\Wikipedia;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
+it('can refresh artists cache', function () {
+    Artist::factory(48)
+        ->state(new Sequence(
+            [],
+            ['discogs' => null, 'filmpolski' => null, 'wikipedia' => null],
+        ))
+        ->create();
+
+    Discogs::shouldReceive('refreshCache')->times(24);
+    FilmPolski::shouldReceive('refreshCache')->times(24);
+    Wikipedia::shouldReceive('refreshCache')->times(24);
+
+    RefreshArtistsCache::dispatchNow();
+});
