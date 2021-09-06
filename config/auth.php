d089f0ac26ec6ceaac520b7a72e408d39cb504fc
Jeremiasz Major <jrh.mjr@gmail.com>
2021-09-06 21:56:03 +0200

    update skeleton

--- config/auth.php
+++ config/auth.php
@@ -12,12 +12,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
-        'api' => [
-            'driver' => 'token',
-            'provider' => 'users',
-            'hash' => false,
-        ],
     ],
 
     'providers' => [
