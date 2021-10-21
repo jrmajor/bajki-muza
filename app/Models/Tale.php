49456fbea0f5bf3ecec2c018fcad112ed6b635a9
Jeremiasz Major <jrh.mjr@gmail.com>
2021-10-21 22:01:02 +0200

    formatting

--- app/Models/Tale.php
+++ app/Models/Tale.php
@@ -142,7 +142,8 @@ final class Tale extends Model
 
     public function scopeWithActorsPopularity(Builder $query): void
     {
-        $query->addSelect(['popularity' => DB::table(
+        $query->addSelect([
+            'popularity' => DB::table(
                 DB::table('tales_actors')
                     ->selectSub(
                         DB::table('tales_actors', 'appearances')
