338e19542c44f637d3ab9e15367617c44a9e82fb
Jeremiasz Major <jrh.mjr@gmail.com>
2021-12-30 19:51:17 +0100

    use LazilyRefreshDatabase trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,14 @@ namespace Tests;
 use App\Images\Jobs\ProcessesImages;
 use App\Models\User;
 use Closure;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Spatie\TemporaryDirectory\TemporaryDirectory;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use LazilyRefreshDatabase;
 
     public function asUser(?User $user = null, ?string $driver = null): TestCase
     {
