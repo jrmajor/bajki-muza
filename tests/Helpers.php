8f02853303b82bf7aabf7d9c90d9c156334c3685
Jeremiasz Major <jrh.mjr@gmail.com>
2021-12-30 19:54:05 +0100

    improve test helpers

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -3,13 +3,24 @@
 namespace Tests;
 
 use App\Models\User;
+use Psl\File;
+use Psl\Str;
 
 function asUser(?User $user = null, ?string $driver = null): TestCase
 {
     return test()->asUser($user, $driver);
 }
 
+/**
+ * @return non-empty-string
+ */
 function fixture(string $path = ''): string
 {
-    return __DIR__ . '/Fixtures' . ($path ? '/' . $path : $path);
+    /** @var non-empty-string */
+    return Str\strip_suffix(__DIR__ . "/Fixtures/{$path}", '/');
+}
+
+function read_fixture(string $path): string
+{
+    return File\read(fixture($path));
 }
