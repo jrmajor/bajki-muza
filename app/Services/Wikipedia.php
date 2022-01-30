bf0b5db8475dceacfccd0f627c63827cbeb502fd
Jeremiasz Major <jrh.mjr@gmail.com>
2022-01-30 02:37:57 +0100

    formatting

--- app/Services/Wikipedia.php
+++ app/Services/Wikipedia.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Values\Wikipedia\Artist;
 use Carbon\CarbonInterval;
-use Exception;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Psl\Hash;
