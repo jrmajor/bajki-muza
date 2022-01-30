10410ef2aa06a38c1b3df33715f8fdf236410fe2
Jeremiasz Major <jrh.mjr@gmail.com>
2022-01-30 01:30:54 +0100

    add breeze version constant

--- app/Application.php
+++ app/Application.php
@@ -7,4 +7,6 @@ use Illuminate\Foundation\Application as BaseApplication;
 class Application extends BaseApplication
 {
     public const SKELETON_VERSION = '8.6.10';
+
+    public const BREEZE_VERSION = '1.7.1';
 }
