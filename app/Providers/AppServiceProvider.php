955c726126f1203f651d5a0ab2fd7ae3be19a64a
Jeremiasz Major <jrh.mjr@gmail.com>
2022-11-05 14:42:29 +0100

    enable eloquent strict mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        Relation::requireMorphMap();
+
+        $shouldBeStrict = ! $this->app->environment('production');
+
+        Model::preventLazyLoading($shouldBeStrict);
+        // Model::preventSilentlyDiscardingAttributes($shouldBeStrict);
+        Model::preventAccessingMissingAttributes($shouldBeStrict);
+    }
 }
