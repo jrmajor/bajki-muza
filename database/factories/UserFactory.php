d655f91982add5cc387f5bc78fa75bef6b710a28
Jeremiasz Major <jrh.mjr@gmail.com>
2022-11-22 18:20:23 +0100

    update skeleton

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'username' => $this->faker->userName(),
+            'username' => $this->faker->unique()->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-        'resources/css/style.css',
-        'resources/js/app.js',
-      ],
+      input: ['resources/css/style.css', 'resources/js/app.js'],
       refresh: true,
     }),
   ],
