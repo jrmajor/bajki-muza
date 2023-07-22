f897a41922b31277dc508e23c8180a8ebabd881b
Jeremiasz Major <jrh.mjr@gmail.com>
2023-07-22 21:30:54 +0200

    update config to default values

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'default',
+            'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
             'block_for' => null,
             'after_commit' => false,
