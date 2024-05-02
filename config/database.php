a8106e1be401b0ae6da20c3f4fb17536273583b5
Jeremiasz Major <jrh.mjr@gmail.com>
2024-05-02 09:18:23 +0200

    replace mysql with sqlite

--- config/database.php
+++ config/database.php
@@ -2,33 +2,24 @@
 
 return [
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'default'),
 
     'connections' => [
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+        'default' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'foreign_key_constraints' => true,
         ],
 
         'testing' => [
             'driver' => 'sqlite',
-            'database' => storage_path('testing.sqlite'),
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('testing.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => true,
         ],
 
     ],
