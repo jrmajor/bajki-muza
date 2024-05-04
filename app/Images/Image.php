8252a9d98518d21ec260a47fbf51462e1059d211
Jeremiasz Major <jrh.mjr@gmail.com>
2024-05-04 13:25:33 +0200

    throw on filesystem errors

--- app/Images/Image.php
+++ app/Images/Image.php
@@ -4,7 +4,6 @@ namespace App\Images;
 
 use App\Images\Exceptions\OriginalDoesNotExist;
 use Carbon\Carbon;
-use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Http\File as LaravelFile;
@@ -51,9 +50,7 @@ abstract class Image extends Model
 
         $path = static::disk()->putFile(static::uploadPath(), $file, 'private');
 
-        if ($path === false) {
-            throw new Exception('Failed to safe a file.');
-        }
+        assert(is_string($path));
 
         /** @var static */
         return tap(
--- config/filesystems.php
+++ config/filesystems.php
@@ -9,7 +9,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'throw' => false,
+            'throw' => true,
         ],
 
         'public' => [
@@ -17,7 +17,7 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
+            'throw' => true,
         ],
 
         'media' => [
@@ -28,7 +28,7 @@ return [
             'bucket' => env('S3_BUCKET_MEDIA'),
             'endpoint' => env('S3_ENDPOINT'),
             'use_path_style_endpoint' => false,
-            'throw' => false,
+            'throw' => true,
         ],
 
         'backup' => [
@@ -39,7 +39,7 @@ return [
             'bucket' => env('S3_BUCKET_BACKUP'),
             'endpoint' => env('S3_ENDPOINT'),
             'use_path_style_endpoint' => false,
-            'throw' => false,
+            'throw' => true,
         ],
 
     ],
