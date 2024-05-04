fd2d9e3025ec8e7e3a2d2ea39c21ad767f51d6ab
Jeremiasz Major <jrh.mjr@gmail.com>
2024-05-04 13:15:22 +0200

    prune telscope entries daily

--- routes/console.php
+++ routes/console.php
@@ -3,10 +3,12 @@
 use App\Console\Commands;
 use Illuminate\Console\Scheduling\Schedule as RealSchedule;
 use Illuminate\Support\Facades\Schedule;
+use Laravel\Telescope\Console\PruneCommand as PruneTelescopeEntries;
 
 Schedule::command(Commands\RefreshArtistsCache::class)
     ->days([RealSchedule::TUESDAY, RealSchedule::FRIDAY])
     ->at('03:00')
     ->graceTimeInMinutes(30);
 
+Schedule::command(PruneTelescopeEntries::class)->dailyAt('01:30');
 Schedule::command(Commands\MakeBackup::class)->dailyAt('02:00');
