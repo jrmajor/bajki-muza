70a058458233599897a3861b89aa11c2f149eea4
Jeremiasz Major <jrh.mjr@gmail.com>
2024-07-14 01:33:07 +0200

    add interfaces for laravel pagination

---
+++ resources/js/types/laravel.d.ts
@@ -0,0 +1,27 @@
+interface PaginatedResource<Resource> {
+	data: Array<Resource>;
+	links: PaginationLinks;
+	meta: PaginationMeta;
+}
+
+interface PaginationLinks {
+	first: string;
+	last: string;
+	prev: string | null;
+	next: string | null;
+}
+
+interface PaginationMeta {
+	current_page: number;
+	from: 1;
+	last_page: 6;
+	links: Array<{
+		active: boolean;
+		label: string;
+		url: string | null;
+	}>;
+	path: string;
+	per_page: number;
+	to: number;
+	total: number;
+}
