4756da64da8d60fda583b1aafcd97a44c5ab3347
Jeremiasz Major <jrh.mjr@gmail.com>
2024-07-14 16:32:33 +0200

    use inertia links in main menu

--- resources/js/Layouts/MainMenuButton.svelte
+++ resources/js/Layouts/MainMenuButton.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { type Snippet } from 'svelte';
+	import { inertia } from '@inertiajs/svelte';
 
 	let { href, children }: { href: string; children: Snippet } = $props();
 </script>
 
-<a {href} class="
+<a {href} use:inertia class="
 	px-5 py-2.5 bg-white dark:bg-gray-700 shadow-lg rounded-full
 	text-gray-900 dark:text-white transition-colors-shadow duration-200 uppercase font-semibold tracking-wide
 	focus:outline-none hover:ring focus:ring active:ring ring-brand-primary hover:ring-opacity-50 focus:ring-opacity-75 active:ring-opacity-100
