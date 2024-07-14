76dabb68c136bbfe686844da7db0053f155aa525
Jeremiasz Major <jrh.mjr@gmail.com>
2024-07-14 20:49:54 +0200

    add ArtistName component

---
+++ resources/js/Components/ArtistName.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { route } from 'ziggy-js';
+	import { type Loop } from '@/helpers/addLoop';
+	import Appearances from '@/Components/Appearances.svelte';
+
+	let {
+		artist,
+		loop,
+		before = '',
+		genetivus = false,
+	}: {
+		artist: {
+			slug: string;
+			name: string;
+			genetivus?: string | null;
+			appearances: number;
+		};
+		loop: Loop;
+		before?: string;
+		genetivus?: boolean;
+	} = $props();
+</script>
+
+<span class="whitespace-nowrap">
+	{#if loop.isFirst}
+		{before}
+	{:else if loop.isLast}
+		&#32;i
+	{/if}
+
+	<a href="{route('artists.show', { artist })}" class="inline-flex items-center">
+		{genetivus ? artist.genetivus ?? artist.name : artist.name}
+		<Appearances count={artist.appearances} small/>
+	</a>{loop.remaining >= 2 ? ',' : ''}
+</span>
