51f8b99e31e25a193e4cbb479e6d5ff40033d9e9
Jeremiasz Major <jrh.mjr@gmail.com>
2024-08-12 22:33:14 +0200

    do not add csrf token to head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="theme-color" content="#ebebeb" media="(prefers-color-scheme: light)">
     <meta name="theme-color" content="#121212" media="(prefers-color-scheme: dark)">
