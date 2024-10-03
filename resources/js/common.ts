7c51bb499e8724f10b8ecbb2f4dcbb941cbd7835
Jeremiasz Major <jrh.mjr@gmail.com>
2024-10-03 21:45:06 +0200

    fix inertia page glob pattern

--- resources/js/common.ts
+++ resources/js/common.ts
@@ -5,7 +5,7 @@ import Layout from '@/Layouts/Layout.svelte';
 export async function resolve(name: string) {
 	const page = await resolvePageComponent(
 		`./Pages/${name}.svelte`,
-		import.meta.glob<{ default: Component }>('./Pages/**/*.svelte'),
+		import.meta.glob<{ default: Component }>('./Pages/*/*.svelte'),
 	);
 	return { default: page.default, layout: Layout };
 }
