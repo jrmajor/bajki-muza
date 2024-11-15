2581dfb16836529948c87b5c8d57f5664d710ee1
Jeremiasz Major <jrh.mjr@gmail.com>
2024-11-15 10:55:00 +0100

    bump inertia to v2

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,5 +1,10 @@
+import { render } from 'svelte/server';
 import { createInertiaApp } from '@inertiajs/svelte';
 import createServer from '@inertiajs/svelte/server';
 import { resolve } from './common';
 
-createServer((page) => createInertiaApp({ page, resolve }));
+createServer((page) => createInertiaApp({
+	page,
+	resolve,
+	setup: ({ App, props }) => render(App, { props }),
+}));
