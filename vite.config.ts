007e276be75a1fd3ee7ff64992324ba9e33a4000
Jeremiasz Major <jrh.mjr@gmail.com>
2025-01-02 00:04:45 +0100

    allow inline assets in vite

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,9 @@ export default defineConfig({
 		tailwindcss(),
 		inertia('resources/js/viteSsr.ts'),
 	],
+	build: {
+		assetsInlineLimit: 4096,
+	},
 	resolve: {
 		alias: {
 			'ziggy-js': path.resolve(__dirname, 'vendor/tightenco/ziggy'),
