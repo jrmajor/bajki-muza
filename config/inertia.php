3bcfcb4d0724dd6e8400add3622276c3537e9f7d
Jeremiasz Major <jrh.mjr@gmail.com>
2026-04-01 20:55:33 +0200

    fix inertia warning

--- config/inertia.php
+++ config/inertia.php
@@ -5,7 +5,7 @@ return [
         'enabled' => (bool) env('INERTIA_SSR_ENABLED', true),
         'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
         'ensure_bundle_exists' => (bool) env('INERTIA_SSR_ENSURE_BUNDLE_EXISTS', true),
-        'bundle' => base_path('bootstrap/ssr/ssr.js'),
+        'bundle' => base_path('bootstrap/ssr/app.js'),
         'throw_on_error' => (bool) env('INERTIA_SSR_THROW_ON_ERROR', false),
     ],
 
